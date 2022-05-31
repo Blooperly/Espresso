dcd0d9c984593debe59a4b0ccb1d634514e1e075
Blooperly <jbrownfisher@gmail.com>
2022-05-31 01:49:31 -0700

    Imported bluetooth example

--- main/h/bluetooth.h
+++ main/h/bluetooth.h
@@ -1,6 +1,35 @@
 #pragma once
 
+typedef struct {
+    uint8_t                 *prepare_buf;
+    int                     prepare_len;
+} prepare_type_env_t;
+
 void BLUETOOTH_init();
-void BLUETOOTH_task();
-void BLUETOOTH_gatts_event();
-void BLUETOOTH_gap_event();
+void BLUETOOTH_task(void* task_params);
+void BLUETOOTH_gap_event(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+void BLUETOOTH_gatts_event(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+static void BLUETOOTH_profile_event(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+void example_write_event_env(esp_gatt_if_t gatts_if, prepare_type_env_t *prepare_write_env, esp_ble_gatts_cb_param_t *param);
+void example_exec_write_event_env(prepare_type_env_t *prepare_write_env, esp_ble_gatts_cb_param_t *param);
+
+struct gatts_profile_inst {
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+};
+
+enum GATTS_APP_LIST {
+    GATTS_APP,
+    GATTS_APP_LAST
+};
